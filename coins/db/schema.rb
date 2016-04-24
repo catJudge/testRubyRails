ac77a5cd538cab744a84e2cdd202ddfb4a81a763
Dmitry Ovchinnikov <dmitry.o.ovchinnikov@gmail.com>
2016-04-24 22:20:48 +0300

    init

--- coins/db/schema.rb
+++ coins/db/schema.rb
@@ -11,23 +11,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405135652) do
+ActiveRecord::Schema.define(version: 20160424191002) do
 
-  create_table "articles", force: :cascade do |t|
+  create_table "blogs", force: :cascade do |t|
     t.string   "title"
-    t.text     "text"
+    t.string   "text"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.string   "commenter"
-    t.text     "body"
-    t.integer  "article_id"
+  add_index "blogs", ["user_id"], name: "index_blogs_on_user_id"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "text"
+    t.integer  "user_id"
+    t.integer  "blog_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "posts", ["blog_id"], name: "index_posts_on_blog_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,     null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "is_admin",               default: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
